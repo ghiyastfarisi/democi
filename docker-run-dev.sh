bde09be83944160d895d7f181657472f93fea036
ghiyastfarisi <ghiyast.farisi@gmail.com>
2019-08-05 21:19:43 +0700

    update default port to 8081 and update yarn dep

--- docker-run-dev.sh
+++ docker-run-dev.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --name democi -p 80:80 -e CI_ENV=development -v $(pwd):/var/www/html democi:v1
+docker run -it --rm --name democi -p 8081:80 -e CI_ENV=development -v $(pwd):/var/www/html democi:v1
